42b04b14bb3fa4aa6bc819ef07ac62c9e20ca2cd
RealMG <realmg51@gmail.com>
2026-04-15 21:24:16 +0700

    feat: add `StringCooker.kt` at `src/kotlin/normal`

---
+++ src/kotlin/normal/StringCooking.kt
@@ -0,0 +1,8 @@
+// nấu "string"
+fun main()
+{
+  val filename = "realmg.kt"
+  val name = filename.substringBefore(".") // expected result: "realmg"
+  val extension = filename.substringAfter(".") // expected result: "kt"
+  println("$filename info: $name, extension: $extension")
+}
