4fc67a0fd4abbdd7a1727d5fca3daca6ac569896
RealMG <realmg51@gmail.com>
2026-04-15 22:18:08 +0700

    format: reformat code (i just think it's c#)

--- src/kotlin/normal/StringCooking.kt
+++ src/kotlin/normal/StringCooking.kt
@@ -1,6 +1,5 @@
 // nấu "string"
-fun main()
-{
+fun main(){
   val filename = "realmg.kt"
   val name = filename.substringBefore(".") // expected result: "realmg"
   val extension = filename.substringAfter(".") // expected result: "kt"
