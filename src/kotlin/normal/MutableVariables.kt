53336f537bf9e839d83a5fbe840d3c5e4f5a440b
RealMG <realmg51@gmail.com>
2026-04-15 22:29:05 +0700

    feat: add `MutableVariables`
    Demonstrate mutable variable usage in Kotlin.

---
+++ src/kotlin/normal/MutableVariables.kt
@@ -0,0 +1,9 @@
+// Muteable variable help you can change ingredients on it
+// let cook to know what is it!
+fun main(){
+  var mutableName = "realmg"
+  println(mutableName)
+  mutableName = "someone"
+  println(mutableName)
+  // DO NOT try to change immutable variable (val)
+}
