7f0cb149b590cc211a3cd6910959c1e1ca071ca5
RealMG <realmg51@gmail.com>
2026-04-16 14:01:22 +0700

    Add IfElse.kt for conditional action demonstration

---
+++ src/kotlin/normal/IfChef/IfElse.kt
@@ -0,0 +1,9 @@
+// Sometimes you need to choose between two actions:if (this) do (it) else do (something else)
+fun main() {
+  val isSalty = true
+  if (isSalty) {
+    println ("Oh,too salty,I need to do something")
+  } else {
+    println ("well,good job boy!")
+  }
+}
