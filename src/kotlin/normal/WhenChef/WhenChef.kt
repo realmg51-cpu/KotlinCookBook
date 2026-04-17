cc9d24785c9865c4c2276130e9441b1e30df8201
RealMG <realmg51@gmail.com>
2026-04-17 17:25:56 +0700

    feat: add `WhenChef` to learn `when expression`

---
+++ src/kotlin/normal/WhenChef/WhenChef.kt
@@ -0,0 +1,14 @@
+// "when" in Kotlin is a yummy food from simple and easy to cook recipe.
+// in this recipe,we will learn how to make IfChef easier and smarter with WhenChef: when expression!
+fun main() {
+  val EggLeft = 20 // you must use PascalCase or camelCase instead 'Egg_Left'
+  when (EggLeft) {
+    1,2,3,4,5,6,7,8,9,10 -> println ("I think I need to buy more")
+    11,12,13,14,15,16,17,18,19,20 -> println ("That's enough,but I must buy more")
+    else -> println ("too much! Hmm... Can't count")
+    // or use this to shorter (range checker)
+    /*
+    in 1..10 -> println ("I think I need to buy more")
+    in 11..20 -> println ("That's enough!")
+    */
+  }
